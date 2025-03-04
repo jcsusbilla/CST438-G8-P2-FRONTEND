4edd2c3fac02dc9cff3b7b30d8dc7ea40dcd19ba
PimentelEthan <epimentel@csumb.edu>
2025-03-04 15:10:05 -0800

    Welcome Page

--- TierList/app/index.tsx
+++ TierList/app/index.tsx
@@ -1,25 +1,68 @@
-import { Text, View, TouchableOpacity, Image } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
-import appStyles from "./styles/appStyles.js";
 
 export default function LandingScreen() {
   const router = useRouter();
-  
+
   return (
-    <View style={appStyles.container}>
-
-      <Text style={appStyles.title}>Tier List</Text>
-
-      {/* Navigate to Signup */}
-      <TouchableOpacity style={appStyles.createAccountButton} onPress={() => router.push("/SignUp")}>
-        <Text style={appStyles.buttonText}>Create an Account</Text>
-      </TouchableOpacity>
-
-      {/* Navigate to Login */}
-      <TouchableOpacity style={[appStyles.createAccountButton, appStyles.loginButton] } onPress={() => router.push("/Login")}>
-        <Text style={appStyles.buttonText}>Log In</Text>
-      </TouchableOpacity>
-
+    <View style={styles.container}>
+      <Text style={styles.title}>Tier List App</Text>
+      <Text style={styles.subtitle}>Create and share tier lists.</Text>
+      
+      <View style={styles.buttonContainer}>
+        {/* Navigate to Login */}
+        <TouchableOpacity style={styles.button} onPress={() => alert("Login Clicked") }>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        
+        {/* Navigate to Sign Up */}
+        <TouchableOpacity style={[styles.button, styles.signupButton]} onPress={() => alert("Sign up Clicked") }>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#2c3e50",
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: "#2c3e50",
+  },
+  buttonContainer: {
+    flexDirection: "column-reverse",
+    justifyContent: "space-between",
+    width: "50%",
+    paddingHorizontal: 30,
+  },
+  button: {
+    backgroundColor: "#3498db",
+    padding: 15,
+    borderRadius: 10,
+    margin: 5,
+    flex: 1,
+    alignItems: "center",
+  },
+  signupButton: {
+    backgroundColor: "#2ecc71",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
