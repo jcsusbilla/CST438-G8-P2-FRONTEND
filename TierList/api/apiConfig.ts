ff087811c3b1c8e7605e359f6ceb5142530a5b9f
jcsusbilla <jcsusbilla@gmail.com>
2025-03-05 23:02:08 -0800

    User backend

---
+++ TierList/api/apiConfig.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const API_URL = "https://tier-list-app-2c41fcb37475.herokuapp.com/";
+export default API_URL;
--- TierList/app/Landing.tsx
+++ TierList/app/Landing.tsx
@@ -1,46 +1,67 @@
 import React from "react";
 import { useState } from 'react';
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { Text, View, TouchableOpacity, TextInput} from "react-native";
 import appStyles from "./styles/appStyles.js";
+import API_URL from "@/api/apiConfig";
 
 
 export default function LandingScreen() {
     const router = useRouter();
+    const { username, firstName, lastName, email } = useLocalSearchParams();
     
+    // LOGOUT FUNCTION
+    const handleLogout = async () => {
+        try {
+            const response = await fetch(`${API_URL}/logout`, {
+                method: 'GET',
+                credentials: 'include'                                                      // important to pass session cookie
+            });
+    
+            if (response.ok) {
+                alert('Logged out successfully!');
+                router.push('/');
+            } else {
+                alert('Logout failed.');
+            }
+        } catch (error) {
+            alert('Logout error');
+        }
+    };
+
     return (
         <View style={appStyles.container}>
-            <Text style={appStyles.title}>Hello, username</Text>
-                {/* Modify Account Button */}
-                <TouchableOpacity style={appStyles.createAccountButton} onPress={() => router.push("/Account")}>
-                    <Text style={appStyles.buttonText}>ACCOUNT</Text>
-                </TouchableOpacity>
+            <Text style={appStyles.title}>Hello, {`${firstName} ${lastName}`}</Text>
+            {/* Modify Account Button */}
+            <TouchableOpacity style={appStyles.createAccountButton} onPress={() => router.push("/Account")}>
+                <Text style={appStyles.buttonText}>ACCOUNT</Text>
+            </TouchableOpacity>
 
-                {/* Show current active tier list */}
-                {/* open a modal */}
+            {/* Show current active tier list */}
+            {/* open a modal */}
 
-                {/* Tier List Page */}
-                <TouchableOpacity style={appStyles.createAccountButton}onPress={() => router.push("/TierList")}>
-                    <Text style={appStyles.buttonText}>TIER LIST PAGE</Text>
+            {/* Tier List Page */}
+            <TouchableOpacity style={appStyles.createAccountButton}onPress={() => router.push("/TierList")}>
+                <Text style={appStyles.buttonText}>TIER LIST PAGE</Text>
 
-                </TouchableOpacity>
+            </TouchableOpacity>
 
-                {/* View all lists */}
-                <TouchableOpacity style={appStyles.createAccountButton}>
-                    <Text style={appStyles.buttonText}>ALL LISTS</Text>
-                    {/* open modal or new page? */}
-                </TouchableOpacity>
+            {/* View all lists */}
+            <TouchableOpacity style={appStyles.createAccountButton}>
+                <Text style={appStyles.buttonText}>ALL LISTS</Text>
+                {/* open modal or new page? */}
+            </TouchableOpacity>
 
-                {/* View public lists */}
-                <TouchableOpacity style={appStyles.createAccountButton}>
-                    <Text style={appStyles.buttonText}>PUBLIC LISTS</Text>
-                    {/* open modal or new page? */}
-                </TouchableOpacity>
+            {/* View public lists */}
+            <TouchableOpacity style={appStyles.createAccountButton}>
+                <Text style={appStyles.buttonText}>PUBLIC LISTS</Text>
+                {/* open modal or new page? */}
+            </TouchableOpacity>
 
-                {/* Logout button */}
-                <TouchableOpacity style={[appStyles.createAccountButton, appStyles.loginButton]} onPress={() => router.push("/")}>
-                    <Text style={appStyles.buttonText}>LOGOUT</Text>
-                </TouchableOpacity>
+            {/* Logout button */}
+            <TouchableOpacity style={[appStyles.createAccountButton, appStyles.loginButton]} onPress={() => router.push("/")}>
+                <Text style={appStyles.buttonText}>LOGOUT</Text>
+            </TouchableOpacity>
         </View>
     );
 }
